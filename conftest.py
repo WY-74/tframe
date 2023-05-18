17028993ac6e05c3aad1aa6456927c1f0cc297e0
WangYun <294961525@qq.com>
2023-05-18 11:40:22 +0800

    230518: add headless

--- conftest.py
+++ conftest.py
@@ -7,13 +7,17 @@ ENV = {}
 def pytest_addoption(parser: Parser):
     mygroup = parser.getgroup(name="iframe")
     mygroup.addoption("--browser", default="Chrome", help="Which browser driver to start")
-    mygroup.addoption("--debugger", default=False, help="Whether to enable debug mode")
+    mygroup.addoption("--debugger", default=False, help="Start debug mode")
+    mygroup.addoption("--headless", default=False, help="No browser visualization interface enabled")
 
 
 def pytest_configure(config: Config):
     browser = config.getoption("--browser")
     debug = config.getoption("--debugger")
+    headless = config.getoption("--headless")
     print(f"{browser} driver has been started")
     print(f"debug mode: {debug}")
+    print(f"headless: {headless}")
     ENV["browser"] = browser
     ENV["debugger"] = debug
+    ENV["headless"] = headless
--- utils/instantiate_driver.py
+++ utils/instantiate_driver.py
@@ -4,9 +4,13 @@ from conftest import ENV
 
 def instantiate_driver():
     browser = ENV["browser"]
+
     options = getattr(webdriver, f"{browser}Options")()
     options.page_load_strategy = 'eager'
-
+    if ENV["headless"]:
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
     if ENV["debugger"]:
         options.debugger_address = "localhost:9000"
 
